7c895b2fc4b3410cafdbdf736abe220de65e774b
Wilfredo Raul Garcia Lujano <wilfredgarcia1997@gmail.com>
2025-02-23 11:40:16 -0600

    Completed coffee machine java basics

---
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeProducts.java
@@ -0,0 +1,8 @@
+package machine;
+
+public final class CoffeeProducts {
+    Ingredients espresso = new Ingredients(0,16,250,4);
+    Ingredients latte = new Ingredients(75,20,350,7);
+    Ingredients cappuccino = new Ingredients(100,12,200,6);
+
+}
